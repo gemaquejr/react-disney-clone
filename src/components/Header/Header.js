6ddab760743a0961fbfe54fa4b861dda49208e7a
gemaquejr <gemaquejr@hotmail.com>
2023-11-13 09:48:20 -0300

    Adiciona tag Home em NavBar
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,10 @@ const Header = (props) => {
                 <img src="/images/logo.svg" alt="Disney+" />
             </Logo>
             <NavMenu>
-                MENU                
+                <a href="/home">
+                    <img src="/images/home-icon.svg" alt="Disney+" />
+                    <span>HOME</span>       
+                </a>
             </NavMenu>
         </Nav>
     );
@@ -54,9 +57,57 @@ const NavMenu = styled.div`
     margin-right: auto;
     margin-left: 25px;
 
-    @media (max-width: 760px) {
-        display: none;
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+        height: 20px;
+        min-width: 20px;
+        width: 20px;
+        z-index: auto;
+        }
+
+        span {
+        color: rgb(249, 249, 249);
+        font-size: 13px;
+        letter-spacing: 1.42px;
+        line-height: 1.08;
+        padding: 2px 0px;
+        white-space: nowrap;
+        position: relative;
+
+        &:before {
+            background-color: rgb(249, 249, 249);
+            border-radius: 0px 0px 4px 4px;
+            bottom: -6px;
+            content: "";
+            height: 2px;
+            left: 0px;
+            opacity: 0;
+            position: absolute;
+            right: 0px;
+            transform-origin: left center;
+            transform: scaleX(0);
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+            visibility: hidden;
+            width: auto;
+        }
     }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export default Header;
