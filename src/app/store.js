1dbd0ce4055798a2ff06bc56710ba9e695a622ce
gemaquejr <gemaquejr@hotmail.com>
2023-11-16 10:32:42 -0300

    Adiciona movieReducer em store
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import useReducer from '../features/user/userSlice';
+import movieReducer from '../features/movie/movieSlice';
 
 const store = configureStore({
   reducer: {
     user: useReducer,
+    movie: movieReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
